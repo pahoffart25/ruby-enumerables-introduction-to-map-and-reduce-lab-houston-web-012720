f230f322f70bc8c25f5a33a778214170ad1f20b9
Paul Hoffart <pahoffart25@gmail.com>
2019-12-11 17:08:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ end
 def reduce_to_all_true(array)
   counter = 0
   while counter < array.size do
-      return false if array.any? = false
+      return false if array.any? == false
       counter += 1
     end
     return true
